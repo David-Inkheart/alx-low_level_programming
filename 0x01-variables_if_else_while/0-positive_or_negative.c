2581822854e364fc29aacf3299e228f012866555
David-Inkheart <okoliedave1@gmail.com>
2022-07-01 17:14:30 -0700

    assign a random number and declare +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,13 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'generating a random number'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;

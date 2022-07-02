f72a95c227d0fb5b7f60050ea543df9ac7edfd5c
David-Inkheart <okoliedave1@gmail.com>
2022-07-01 19:04:15 -0700

    print all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'generating a random number'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 0;
+
+while (a < 100)
+{
+	b = a % 10;
+	c = a / 10;
+	
+if (c < b)
+{
+putchar(c + '0');
+putchar(b + '0');
+
+if (a < 89)
+{
+putchar(44);
+putchar(32);
+}
+}
+
+	a++;
+}
+putchar('\n');
+
+return (0);
+}

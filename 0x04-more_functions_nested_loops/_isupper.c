73d473afafec8dcad1c42956b7ee770669c306a5
David-Inkheart <okoliedave1@gmail.com>
2022-07-07 16:59:50 -0700

    a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/_isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @c: The character to print
+ * Description: 'This program prints the alphabet in lowercase'
+ * The printout is followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Header file
+ *
+ *
+ */
+int _isupper(int c);
+
+#endif /* MAIN_H */

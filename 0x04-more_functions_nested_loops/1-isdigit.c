9513d0c618e833c21bd092736a7953754489faf3
David-Inkheart <okoliedave1@gmail.com>
2022-07-07 17:11:19 -0700

    a function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for lowercase character c
+ * @c: The character to print
+ * int c - a random number from '0' to '9''
+ * Return: 1 if character is lowercase, 0 if not.
+ */
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+	return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @c: The integer to check
+ * Description: 'This program prints the alphabet in lowercase'
+ * The printout is followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	return (0);
+}

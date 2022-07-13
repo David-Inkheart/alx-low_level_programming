2e335d34baa7171ebd55fe898d8a11c26c2ca727
David-Inkheart <okoliedave1@gmail.com>
2022-07-12 17:51:16 -0700

    updating the value a pointer points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* main - entry point
+* reset_to_98 - function that updates the pointer of an integer
+* @n: pointer to n to be updated
 *
-* Return: Always 0.
 */
-int main(void)
+void reset_to_98(int *n)
 {
 	*n = 98;
-	return (0);
 }

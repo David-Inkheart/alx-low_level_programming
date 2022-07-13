5ccdc2c73e9821dd0588c8bbe7fcb14c05b0e67c
David-Inkheart <okoliedave1@gmail.com>
2022-07-13 08:54:42 -0700

    print every other character of a string, starting with the first character.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts2 - writes the string str and a trailing newline to stdout
+*
+* @str: string to be written out to stdout
+*
+* Return: nothing
+*
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= str[i]; i++)
+	{
+	if (i % 2 == 0)
+
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}

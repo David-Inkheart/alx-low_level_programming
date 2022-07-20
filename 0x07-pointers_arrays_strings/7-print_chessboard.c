77d022160c064b3333efea016fc40c65cfbd4c70
David-Inkheart <okoliedave1@gmail.com>
2022-07-19 17:14:52 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,17 +13,13 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; a[i][7]; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			printf("%c ", a[i][j]);
-		}
-		if (j == 7)
-			break;
-		{
-			printf("\n");
-		}
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+
 	}
 }
 

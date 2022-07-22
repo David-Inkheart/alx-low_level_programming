45b0f4b1c6cb9ab8d1ed45650fb5c67169c885f2
David-Inkheart <okoliedave1@gmail.com>
2022-07-22 16:36:38 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - print the result of the multiplication, followed by a new line
+ *
+ * @argc: count of arguments entered, must be >= 1.
+ * @argv: argument vector which is a one dimensional array
+ * which contains as strings - the arguments passed to main
+ *
+ * Return: - If the program does not receive two arguments,
+ * your program should print Error, followed by a new line, and return 1
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc > 1)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	if (argc < 2)
+	{
+		printf("%s\n", "Error");
+
+		return (1);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #define UNUSED(x) (void)(x)
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int main(int argc, char *argv[]);

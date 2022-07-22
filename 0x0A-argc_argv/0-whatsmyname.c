707e5ed30cd44534728cfaa7f5651cae6c9bbc2b
David-Inkheart <okoliedave1@gmail.com>
2022-07-22 14:29:54 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * @argc: count of arguments entered, must be >= 1.
+ * @argv: argument vector which is a one dimensional array
+ * which contains as strings - the arguments passed to main
+ *
+ * Return: - Always 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define UNUSED(x) (void)(x)
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */

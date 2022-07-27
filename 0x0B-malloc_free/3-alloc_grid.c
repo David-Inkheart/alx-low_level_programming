dc90358378112a6d08de790a2d4dafcd9d31fd43
David-Inkheart <okoliedave1@gmail.com>
2022-07-26 17:33:51 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,10 +35,25 @@ int **alloc_grid(int width, int height)
 		}
 	}
 	return (arr);
+}
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created by
+ * my alloc_grid function.
+ *
+ * @grid: pointer to 2D array
+ * @height: row or pointer to string or 1D array
+ *
+ * Return: - nothing
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
 	for (i = 0; i < height; i++)
 	{
-		free(arr[i]);
+		free(grid[i]);
 	}
-	free(arr);
-
+	free(grid);
 }

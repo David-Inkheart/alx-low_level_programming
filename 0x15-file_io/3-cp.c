9f92744b1ec2359fedcd52e49639e43f50a5e520
David-Inkheart <okoliedave1@gmail.com>
2022-08-22 22:08:19 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
@@ -101,9 +99,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security

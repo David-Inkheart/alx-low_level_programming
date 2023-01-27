457cd511ebcc2dc0ce873f3e468ae45a8aac9f21
David-Inkheart <okoliedave1@gmail.com>
2023-01-27 12:37:51 -0800

    searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
@@ -23,12 +23,9 @@ int linear_search(int *array, size_t size, int value)
 		printf("value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			break;
+			return (i);
 		}
 
 	}
-	if (i == size)
-		return (-1);
-
-	return (i);
+	return (-1);
 }
